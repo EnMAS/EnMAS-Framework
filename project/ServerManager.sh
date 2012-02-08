975fe2941ee6adc955da30751dff00aeba1aca82
Connor Doyle <connor@oneorangesoftware.com>
2012-02-07 20:41:07 -0600

    Added long polling of the Server by the Session, the result of which controls a status bar on the Session GUI indicating whether the server is responsive or not.  Rearranged the project folder structure a litle bit as well.

--- project/ServerManager.sh
+++
@@ -1,8 +0,0 @@
-export CLASSPATH=$CLASSPATH:lib_managed/jars/com.typesafe.akka/akka-actor/akka-actor-2.0-M3.jar
-export CLASSPATH=$CLASSPATH:lib_managed/jars/com.typesafe.akka/akka-remote/akka-remote-2.0-M3.jar
-export CLASSPATH=$CLASSPATH:lib_managed/bundles/io.netty/netty/netty-3.3.0.Final.jar
-export CLASSPATH=$CLASSPATH:lib_managed/jars/com.google.protobuf/protobuf-java/protobuf-java-2.4.1.jar
-export CLASSPATH=$CLASSPATH:lib_managed/jars/commons-io/commons-io/commons-io-1.4.jar
-export CLASSPATH=$CLASSPATH:config:target/scala-2.9.1/enmas_2.9.1-0.5.jar
-
-scala -cp $CLASSPATH org.enmas.server.ServerManager
