f10f164499187e519a56dff71aca2b628fefcfd4
Connor Doyle <connor@oneorangesoftware.com>
2012-12-02 22:24:09 -0600

    Added the JavaSarsaAgent to the examples directory.

---
+++ examples/agents/JavaSarsaAgent/JavaSarsaAgent.java
@@ -0,0 +1,78 @@
+import org.enmas.client.Agent;
+import org.enmas.pomdp.State;
+import org.enmas.pomdp.Action;
+
+import java.util.Random;
+import java.util.Map;
+import java.util.HashMap;
+import java.util.Set;
+import java.util.TreeSet;
+import java.util.AbstractMap.SimpleImmutableEntry; // poor man's 2-tuple
+
+/** 
+* Implementes the SARSA reinforcement learning algorithm, a modified
+* version of Q-Learning.
+*/
+public class JavaSarsaAgent extends Agent {
+
+	/** Represents a (State, Action) tuple. */
+	private class Situation extends SimpleImmutableEntry<State, Action> {
+		public Situation(State state, Action action) { super(state, action); }
+	}
+
+	final Random random = new Random();
+	final double randomFactor = 0.1; // this agent acts randomly 10% of the time
+	final float alpha = 0.9f;
+	final float gamma = 0.9f;
+	HashMap<Situation, Float> qTable = new HashMap<Situation, Float>();
+	Situation lastSA = new Situation(State.empty(), Action.DoNothing());
+
+	public String name() {
+		return "Java Sarsa Agent";
+	}
+
+	public Action policy(State observation, float reward) {
+
+		Map<Float, Action> options = new HashMap<Float, Action>();
+		scala.collection.Iterator<Action> it = actions().iterator();
+		while (it.hasNext()) {
+			Action action = it.next();
+			options.put(expectedReward(observation, action), action);
+		}
+
+		Action bestGuess = options.get(
+			new TreeSet<Float>(options.keySet()).last()
+		);
+
+		Action decision = bestGuess;
+
+		if (random.nextInt(10) < 1) { decision = randomAction(); }
+
+		Situation currentSA = new Situation(observation, decision);
+
+		float currentExpected = expectedReward(observation, decision);
+
+		float lastExpected = expectedReward(
+			lastSA.getKey(),
+			lastSA.getValue()
+		);
+
+		float newQValue = lastExpected + alpha * (
+			reward + gamma * currentExpected - lastExpected
+		);
+
+		qTable.put(lastSA, newQValue); // update the Q-Table
+		lastSA = currentSA;
+
+		return decision;
+	}
+
+	float expectedReward(State state, Action action) {
+		Float expected = qTable.get(new Situation(state, action));
+		return (expected != null) ? expected : 0;
+	}
+
+	Action randomAction() {
+		return actions().toSeq().apply(random.nextInt(actions().size()));
+	}
+}
